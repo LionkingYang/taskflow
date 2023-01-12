558ed6676c8461467b3c0f87355efe0d69855699
LionkingYang <1290657017@qq.com>
2023-01-12 14:15:16 +0800

    支持异步任务 (#4)
    * feature:支持异步任务

--- taskflow/include/common_struct/task_struct.h
+++ taskflow/include/common_struct/task_struct.h
@@ -19,7 +19,8 @@ struct Job {
   string op_name;
   string config;
   vector<string> dependencies;
-  KCFG_DEFINE_FIELDS(task_name, op_name, config, dependencies)
+  bool async = false;
+  KCFG_DEFINE_FIELDS(task_name, op_name, config, dependencies, async)
 };
 
 struct Jobs {
@@ -36,10 +37,14 @@ struct TaskContext {
   const std::any global_input;
   std::any* global_output;
   taskflow::ConcurrentMap<string, std::any> task_output;
-  taskflow::ConcurrentMap<string, std::unordered_map<string, string>> task_config;
-  TaskContext(const std::any& input, std::any* output) : global_input(input), global_output(output) {}
+  taskflow::ConcurrentMap<string, std::unordered_map<string, string>>
+      task_config;
+  TaskContext(const std::any& input, std::any* output)
+      : global_input(input), global_output(output) {}
   ~TaskContext() { Clear(); }
   void Clear() { task_output.clear(); }
 };
-using TaskFunc = std::any (*)(taskflow::TaskContext&, const std::vector<std::string>&, const string& task_name);
+using TaskFunc = std::any (*)(taskflow::TaskContext&,
+                              const std::vector<std::string>&,
+                              const string& task_name);
 }  // namespace taskflow
